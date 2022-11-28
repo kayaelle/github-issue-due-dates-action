d438c4ac4fd4fb9844fbf5b6af75e9dd25ed2eff
Kerri Lemoie <klemoie@Kerris-MacBook-Pro.local>
2022-11-28 12:16:58 -0500

    Adding label options

--- src/app.ts
+++ src/app.ts
@@ -18,6 +18,26 @@ export const run = async () => {
     for (const issue of results) {
       const daysUtilDueDate = await datesToDue(issue.due);
 
+      // Between 0 and 1 day until due date
+      if (daysUtilDueDate <= 1 && daysUtilDueDate > 0) {
+        await ok.addLabelToIssue(context.repo.owner, context.repo.repo, issue.number, [TOMORROW_TAG_NAME]);
+      }
+
+      // Between 1 and 2 days until due date
+      if (daysUtilDueDate <= 2 && daysUtilDueDate > 1) {
+        await ok.addLabelToIssue(context.repo.owner, context.repo.repo, issue.number, [TWO_DAYS_TAG_NAME]);
+      }
+
+       // Between 2 and 3 days until due date
+      if (daysUtilDueDate <= 3 && daysUtilDueDate > 2) {
+        await ok.addLabelToIssue(context.repo.owner, context.repo.repo, issue.number, [THREE_DAYS_TAG_NAME]);
+      }
+
+       // Between 3 and 5 days until due date
+      if (daysUtilDueDate <= 5 && daysUtilDueDate > 3) {
+        await ok.addLabelToIssue(context.repo.owner, context.repo.repo, issue.number, [FIVE_DAYS_TAG_NAME]);
+      }
+
       // Between 0 and 7 days until due date
       if (daysUtilDueDate <= 7 && daysUtilDueDate > 0) {
         await ok.addLabelToIssue(context.repo.owner, context.repo.repo, issue.number, [NEXT_WEEK_TAG_NAME]);
--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,6 @@
+export const TOMORROW_TAG_NAME: string = 'Due tomorrow';
+export const TWO_DAYS_TAG_NAME: string = 'Due 2 days';
+export const THREE_DAYS_TAG_NAME: string = 'Due 3 days';
+export const FIVE_DAYS_TAG_NAME: string = 'Due 5 days';
 export const OVERDUE_TAG_NAME: string = 'Overdue';
 export const NEXT_WEEK_TAG_NAME: string = 'Due next week';
